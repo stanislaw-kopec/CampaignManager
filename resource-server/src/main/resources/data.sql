c1df471bb0f7efb1eac8de0ac4510c196e1d3c50
stanislaw-kopec <stachu1325e@gmail.com>
2026-05-13 12:15:56 +0200

    Add data.sql file

---
+++ resource-server/src/main/resources/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO users (username)
+VALUES
+('john_doe'),
+('jane_smith'),
+('mike_johnson'),
+('emily_brown'),
+('chris_wilson');
+
+INSERT INTO emerald_accounts (user_id, balance)
+VALUES
+(1, 10000.00),
+(2, 15250.50),
+(3, 8750.75),
+(4, 20300.00),
+(5, 5400.25);
+
+INSERT INTO towns (name)
+VALUES
+('Warsaw'),
+('Krakow'),
+('Gdansk'),
+('Wroclaw'),
+('Poznan');
+
+INSERT INTO keywords (name)
+VALUES
+('Cars'),
+('Gaming'),
+('Electronics'),
+('Books'),
+('Fashion'),
+('Sport'),
+('Travel'),
+('Music'),
+('Food'),
+('Technology');
